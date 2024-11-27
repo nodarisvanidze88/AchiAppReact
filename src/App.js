2f067e1480f94c0d99ef1cd0d587ea828f869693
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-11-27 23:41:09 +0400

    create first view

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-
 import BackForward from './Components/BackForward';
-import CheckUser from './Components/CheckUser';
+// import CheckUser from './Components/CheckUser';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import AddCustomer from "./Components/AddCustomer"
-import {UsersOrders} from './Components/UsersOrders'
+import AddCustomer from './Components/AddCustomer';
+import { UsersOrders } from './Components/UsersOrders';
 
 function App() {
-  return (
-   <div>
-    <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<CheckUser />}/>
-      <Route path='/backforward/:userValue/:selectdID/:customerName/:customerID' element={<BackForward />} />
-      <Route path='/newCustomer' element={<AddCustomer />} />
-      <Route path='/orders' element={<UsersOrders />} />
-      </Routes>
-    </BrowserRouter>
-   </div>
-  );
+    return (
+        <div>
+            <BrowserRouter>
+                <Routes>
+                    {/* <Route path='/' element={<CheckUser />}/> */}
+                    {/* <Route path='/backforward/:userValue/:selectdID/:customerName/:customerID' element={<BackForward />} /> */}
+                    <Route path="/" element={<BackForward />} />
+                    <Route path="/newCustomer" element={<AddCustomer />} />
+                    <Route path="/orders" element={<UsersOrders />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- src/Components/BackForward.js
+++ src/Components/BackForward.js
@@ -1,21 +1,20 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck, faFloppyDisk } from '@fortawesome/free-solid-svg-icons';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/swiper-bundle.min.css';
-import 'swiper/modules/navigation/navigation.min.css';
-import 'swiper/modules/pagination/pagination.min.css';
-import { Navigation, Pagination } from 'swiper';
-import { URLS } from './urls';
-import NavigationBar from './navBar';
+import { FreeMode, Navigation, Thumbs } from 'swiper/modules';
+// import NavigationBar from './navBar';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+import 'swiper/css/thumbs';
 import './BackForward.css';
+import { URLS } from './urls';
 
 export default function BackForward() {
     const [data, setData] = useState([]);
     const [inputValue, setInputValue] = useState(0);
     const [collectedData, setCollectedData] = useState([]);
-    const [activeIndex, setActiveIndex] = useState(0); // Track active slide index
+    const [thumbsSwiper, setThumbsSwiper] = useState(null); // Thumbnail swiper instance
     const { userValue, selectdID, customerName, customerID } = useParams();
     const navigate = useNavigate();
 
@@ -34,7 +33,9 @@ export default function BackForward() {
     const handleSave = () => {
         if (inputValue > 0) {
             const itemIndex = collectedData.findIndex(
-                (item) => item.product_ID === data[activeIndex].product_id
+                (item) =>
+                    item.product_ID ===
+                    data[thumbsSwiper?.activeIndex]?.product_id
             );
             if (itemIndex !== -1) {
                 const updatedData = [...collectedData];
@@ -44,8 +45,8 @@ export default function BackForward() {
                 const newItem = {
                     user: selectdID,
                     customer_info: parseInt(customerID, 10),
-                    product_ID: data[activeIndex].product_id,
-                    item_name: data[activeIndex].item_name,
+                    product_ID: data[thumbsSwiper?.activeIndex]?.product_id,
+                    item_name: data[thumbsSwiper?.activeIndex]?.item_name,
                     quantity: parseInt(inputValue, 10),
                 };
                 setCollectedData((prevData) => [...prevData, newItem]);
@@ -58,16 +59,14 @@ export default function BackForward() {
         console.log(collectedData);
         fetch(URLS[0].Add_collection_data, {
             method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
+            headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ collected_data: collectedData }),
         }).then((res) => {
             if (res.ok) {
-                console.log('Data Saved');
+                console.log('Data saved');
                 setCollectedData([]);
             } else {
-                console.error('error');
+                console.error('Error saving data');
             }
         });
     };
@@ -76,102 +75,93 @@ export default function BackForward() {
         navigate(`/`);
     };
 
-    const getQuantityForCurrentItem = () => {
-        const currentItem = collectedData.find(
-            (item) => item.product_ID === data[activeIndex].product_id
-        );
-        return currentItem ? currentItem.quantity : 0;
-    };
-
     return (
-        <>
-            <NavigationBar />
-            <div className="container-content">
-                <button onClick={backToCustomer}>Back</button>
-                {data.length > 0 && (
-                    <div className="items-images-buttons">
-                        <Swiper
-                            modules={[Navigation, Pagination]}
-                            navigation
-                            pagination={{ clickable: true }}
-                            onSlideChange={(swiper) =>
-                                setActiveIndex(swiper.activeIndex)
-                            }
-                            className="swiper-container"
-                        >
-                            {data.map((item, index) => (
-                                <SwiperSlide
-                                    key={index}
-                                    className="swiper-slide"
-                                >
-                                    <div className="image-item">
-                                        <img
-                                            src={item.image_urel}
-                                            alt="No"
-                                            height={400}
-                                            width={400}
-                                            className="swiper-image"
-                                        />
-                                        <div className="item-details">
-                                            <span className="item_names">
-                                                დასახელება:{' '}
-                                                <span className="item_name_content">
-                                                    {item.item_name}
-                                                </span>
-                                            </span>
-                                            <span className="item_names">
-                                                კატეგორია:{' '}
-                                                <span className="item_name_content">
-                                                    {item.category_name}
-                                                </span>
-                                            </span>
-                                            <span className="item_names">
-                                                რაოდენობა:{' '}
-                                                <span className="item_name_content">
-                                                    {item.qty_in_wh}{' '}
-                                                    {item.dimention}
-                                                </span>
-                                            </span>
-                                            <span className="item_names">
-                                                ღირებულება:{' '}
-                                                <span className="item_name_content">
-                                                    {item.price} ლარი
-                                                </span>
-                                            </span>
-                                        </div>
+        // <>
+        //     <NavigationBar />
+        <div className="container-content">
+            <button onClick={backToCustomer}>Back</button>
+            {data.length > 0 && (
+                <>
+                    <Swiper
+                        style={{
+                            '--swiper-navigation-color': '#fff',
+                            '--swiper-pagination-color': '#fff',
+                        }}
+                        loop={true}
+                        spaceBetween={10}
+                        navigation={true}
+                        thumbs={{ swiper: thumbsSwiper }}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="mainSwiper"
+                    >
+                        {data.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <div className="card">
+                                    <img
+                                        src={item.image_urel}
+                                        alt={item.item_name}
+                                        className="card-image"
+                                    />
+                                    <div className="item-details">
+                                        <span className="item_names">
+                                            Name: <span>{item.item_name}</span>
+                                        </span>
+                                        <span className="item_names">
+                                            Category:{' '}
+                                            <span>{item.category_name}</span>
+                                        </span>
+                                        <span className="item_names">
+                                            Stock: <span>{item.qty_in_wh}</span>
+                                        </span>
+                                        <span className="item_names">
+                                            Price:{' '}
+                                            <span>{item.price} ლარი</span>
+                                        </span>
                                     </div>
-                                </SwiperSlide>
-                            ))}
-                        </Swiper>
-
-                        <div className="produqct-qt">
-                            <input
-                                type="number"
-                                value={inputValue}
-                                onChange={handleInputChanges}
-                                className="quantity-input"
-                            />
-                            <button
-                                type="submit"
-                                onClick={handleSave}
-                                className="save-button"
-                            >
-                                <FontAwesomeIcon icon={faCheck} size="2xl" />
-                            </button>
-                            <button
-                                type="submit"
-                                onClick={handleFinish}
-                                className="finish-button"
-                            >
-                                <FontAwesomeIcon
-                                    icon={faFloppyDisk}
-                                    size="2xl"
+                                </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <Swiper
+                        onSwiper={setThumbsSwiper}
+                        loop={true}
+                        spaceBetween={10}
+                        slidesPerView={4}
+                        freeMode={true}
+                        watchSlidesProgress={true}
+                        modules={[FreeMode, Navigation, Thumbs]}
+                        className="thumbSwiper"
+                    >
+                        {data.map((item, index) => (
+                            <SwiperSlide key={index}>
+                                <img
+                                    src={item.image_urel}
+                                    alt={item.item_name}
+                                    className="thumbnail-image"
                                 />
-                            </button>
-                        </div>
+                            </SwiperSlide>
+                        ))}
+                    </Swiper>
+                    <div className="input-section">
+                        <input
+                            type="number"
+                            value={inputValue}
+                            onChange={handleInputChanges}
+                            className="quantity-input"
+                        />
+                        <button onClick={handleSave} className="save-button">
+                            Save
+                        </button>
+                        <button
+                            onClick={handleFinish}
+                            className="finish-button"
+                        >
+                            Finish
+                        </button>
                     </div>
-                )}
-            </div>
-        </>
+                </>
+            )}
+        </div>
+        // </>
     );
 }
