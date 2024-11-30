df8555867a1ba4e849b7146e906a5b68de142f04
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-11-30 22:55:34 +0400

    change url

--- src/Components/urls.js
+++ src/Components/urls.js
@@ -3,7 +3,7 @@ export const URLS = [
         Add_collection_data:
             'https://achiapp.pythonanywhere.com/fi/add_collection_data',
         GetUsers: 'https://achiapp.pythonanywhere.com/fi/getusers',
-        All_Items: 'http://127.0.0.1:8000/fi/allItems',
+        All_Items: 'https://achiapp.pythonanywhere.com/fi/allItems',
         All_Customers: 'https://achiapp.pythonanywhere.com/fi/customers/',
     },
 ];
