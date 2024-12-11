0fe18e69d7107444913b50883abd2584acfb9caa
nodarisvanidze88 <nodarisvanidze88@gmail.com>
2024-12-11 07:37:48 +0400

    fix urls

--- src/Components/urls.js
+++ src/Components/urls.js
@@ -1,7 +1,8 @@
 export const URLS = [
     {
-        Add_collection_data: 'http://127.0.0.1:8000/fi/add_collection_data',
-        GetUsers: 'http://127.0.0.1:8000/fi/getusers',
+        Add_collection_data:
+            'https://achiapp.pythonanywhere.com/fi/add_collection_data',
+        GetUsers: 'https://achiapp.pythonanywhere.com/fi/getusers',
         All_Items: 'https://achiapp.pythonanywhere.com/fi/allItems',
         All_Customers: 'https://achiapp.pythonanywhere.com/fi/customers/',
         Category_list: 'https://achiapp.pythonanywhere.com/fi/category_list',
